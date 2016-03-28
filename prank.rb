f41d506e38480fa34241cd4e6da28648bff4871d
Ben Miller <benjaminmiller229@gmail.com>
2016-03-28 01:29:50 -0400

    Update prank.rb

--- prank.rb
+++ prank.rb
@@ -2,11 +2,11 @@ deathChar = " "							      # Alt code death char - ("invisible character")
 
 fileName = "VICTIM_FILE.extension" # Declare the file name that you want destroyed
 file = File.open(fileName)				 # Open the file for modification
-contents = File.read(file)				 # Read contents of the file
-file.close								         # Close file
+contents = File.read(file)				 # Save file contents to temporary variable
+file.close								         
 
 contents = contents.tr(" ",deathChar)	# Replace contents of the file with malicious code
 
 fileModified = File.new(fileName,'w')	# Reopen file for writing
 fileModified.puts(contents)				    # Refill file with new malicious code
-fileModified.close						        # Close file
+fileModified.close						        
